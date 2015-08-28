24966f571868b5d54025f62a4cd1b228a83b0112
lucien <me@lucienbertin.com>
2015-08-28 11:58:20 +0200

    [add] all dem js files yo!

---
+++ js/directives/timespan-picker.js
@@ -0,0 +1,206 @@
+(function () {
+	'use strict';
+
+	angular.module('lui.directives').directive('luidTimespan', ['moment', function (moment) {
+		function link(scope, element, attrs, ctrls) {
+
+			var ngModelCtrl = ctrls[1];
+			var luidTimespanCtrl = ctrls[0];
+			scope.pattern = /^([0-9]+)((h([0-9]{2})?)?(m(in)?)?)?$/i;
+			if (!!attrs.unit) {
+				var unit = scope.$eval(attrs.unit);
+				if (unit == 'h' || unit == 'hour' || unit == 'hours') {
+					scope.useHours = true;
+				}
+			}
+
+			scope.ngModelCtrl = ngModelCtrl;
+
+			ngModelCtrl.$render = function () {
+				if (!this.$viewValue) {
+					scope.strDuration = '';
+					return;
+				}
+
+				var currentDuration = moment.duration(this.$viewValue);
+				var hours = Math.floor(currentDuration.asHours());
+				var minutes = currentDuration.minutes();
+				if (hours === 0) {
+					scope.strDuration = minutes + 'm';
+				} else {
+					scope.strDuration = (hours < 10 ? '0' : '') + hours + 'h' + (minutes < 10 ? '0' : '') + minutes;
+				}
+			};
+
+			// call the ng-change
+			ngModelCtrl.$viewChangeListeners.push(function () {
+				scope.$eval(attrs.ngChange);
+			});
+
+			// bind to various events - here only keypress=enter
+			luidTimespanCtrl.setupEvents(element.find('input'));
+		}
+
+
+		return {
+			require: ['luidTimespan', '^ngModel'],
+			controller: 'luidTimespanController',
+			scope: {
+				step: '=', // default = 5
+				unit: '=', // 'hours', 'hour', 'h' or 'm', default='m'
+				ngDisabled: '=',
+				placeholder: '@'
+			},
+			restrict: 'EA',
+			link: link,
+			template: "<input type='text' ng-disabled='ngDisabled' class='lucca-timespan' placeholder='{{placeholder}}' ng-pattern='pattern' ng-model='strDuration' ng-change='updateValue()' ng-blur='formatInputValue()'>"
+		};
+	}])
+	.controller('luidTimespanController', ['$scope', 'moment', function ($scope, moment) {
+
+		// public methods for update
+		$scope.updateValue = function () {
+			// is only fired when pattern is valid or when it goes from valid to invalid
+			// improvement possible - check the pattern and set the validity of the all directive via ngModelCtrl.$setValidity
+			// currently when pattern invalid, the viewValue is set to '00:00:00'
+			if (!$scope.strDuration) { return updateWithoutRender(undefined); } // empty input => 00:00:00
+
+			// temp variables
+			var newDuration; // the duration of the parsed strDuration
+			var newValue;
+
+			// parse the strDuration to build newDuration
+			newDuration = parse($scope.strDuration);
+
+			// transform this duration into a string
+			newValue = format(newDuration);
+
+			// update viewvalue
+			updateWithoutRender(newValue);
+		};
+		var format = function (dur) {
+			return (dur.days() > 0 ? Math.floor(dur.asDays()) + '.' : '') + (dur.hours() < 10 ? '0' : '') + dur.hours() + ':' + (dur.minutes() < 10 ? '0' : '') + dur.minutes() + ':00';
+		};
+		var parse = function (strInput) {
+			var newDuration;
+			if (/h/i.test(strInput)) {
+				newDuration = parseHoursAndMinutes(strInput);
+			} else if (/m/i.test(strInput)) {
+				newDuration = parseMinutes(strInput);
+			} else if ($scope.useHours) {
+				newDuration = parseHours(strInput);
+			} else {
+				newDuration = parseMinutes(strInput);
+			}
+			return newDuration;
+		};
+
+		// private - parsing str to moment.duration
+		var parseHoursAndMinutes = function (strInput) {
+			var d = moment.duration();
+			var splitted = strInput.split(/h/i);
+			d.add(parseInt(splitted[0]), 'hours');
+			var strMin = splitted[1];
+			if (!!strMin && strMin.length >= 2) {
+				d.add(parseInt(strMin.substring(0, 2)), 'minutes');
+			}
+			return d;
+		};
+		var parseMinutes = function (strInput) {
+			var d = moment.duration();
+			var splitted = strInput.split(/m/i);
+			d.add(parseInt(splitted[0]), 'minutes');
+			return d;
+		};
+		var parseHours = function (strInput) {
+			var d = moment.duration();
+			var splitted = strInput.split(/h/i);
+			d.add(parseInt(splitted[0]), 'hours');
+			return d;
+		};
+
+		// private - formatting stuff
+		var formatValue = function (duration) {
+			return Math.floor(duration.asDays()) + '.' + (duration.hours() < 10 ? '0' : '') + duration.hours() + ':' + (duration.minutes() < 10 ? '0' : '') + duration.minutes() + ':00';
+		};
+
+		// private - updates of some kinds
+		// incr value by `step` minutes
+		var incr = function (step) {
+			var newDur = moment.duration(currentValue()).add(step, 'minutes');
+			if (newDur.asMilliseconds() < 0) {
+				newDur = moment.duration();
+			}
+			var newValue = formatValue(newDur);
+			update(newValue);
+		};
+
+		// sets viewValue and renders
+		var update = function (newValue) {
+			$scope.ngModelCtrl.$setViewValue(newValue);
+			$scope.ngModelCtrl.$render();
+		};
+		var updateWithoutRender = function (newValue) {
+			$scope.ngModelCtrl.$setViewValue(newValue);
+		};
+
+		// display stuff
+		$scope.formatInputValue = function () {
+			$scope.ngModelCtrl.$render();
+		};
+
+		var currentValue = function () {
+			return $scope.ngModelCtrl.$viewValue;
+		};
+
+		// events - key 'enter'
+		this.setupEvents = function (elt) {
+			setupKeyEvents(elt);
+			setupMousewheelEvents(elt);
+		};
+
+		var setupKeyEvents = function (elt) {
+			var step = 5;
+			if (!isNaN(parseInt($scope.step))) {
+				step = parseInt($scope.step);
+			}
+			elt.bind('keydown', function (e) {
+				if (e.which === 38) { // up
+					e.preventDefault();
+					incr(step);
+					$scope.$apply();
+				} else if (e.which === 40) { // down
+					e.preventDefault();
+					incr(-step);
+					$scope.$apply();
+				}
+				if (e.which === 13) { // enter
+					e.preventDefault();
+					$scope.formatInputValue();
+					$scope.$apply();
+				}
+			});
+		};
+		var setupMousewheelEvents = function (elt) {
+			var step = 5;
+			if (!isNaN(parseInt($scope.step))) {
+				step = parseInt($scope.step);
+			}
+			var isScrollingUp = function (e) {
+				if (e.originalEvent) {
+					e = e.originalEvent;
+				}
+				//pick correct delta variable depending on event
+				var delta = (e.wheelDelta) ? e.wheelDelta : -e.deltaY;
+				return (e.detail || delta > 0);
+			};
+
+			elt.bind('mousewheel wheel', function (e) {
+				if (this === document.activeElement) {
+					$scope.$apply(incr((isScrollingUp(e)) ? step : -step));
+					e.preventDefault();
+				}
+			});
+		};
+	}]);
+})();
