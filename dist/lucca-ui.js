4ac178d56e4e185007ecfae7e41d97856aa9e602
lucien <me@lucienbertin.com>
2015-08-28 17:23:45 +0200

    [transpilation]

--- dist/lucca-ui.js
+++ dist/lucca-ui.js
@@ -784,3 +784,37 @@
 		};
 	}]);
 })();
+;(function(){
+	'use strict';
+	angular.module('lui.services')
+	.service('luisNotify', ['$log', 'notify', function($log, notify){
+		var template = 
+			"<div class=\"lui callout filled\" ng-click=\"$close()\" " + 
+			"style=\"width:25em;z-index:999\"" + 
+			"ng-class=\"[$classes, " + 
+			"$position === 'center' ? 'cg-notify-message-center' : '', " +
+			"$position === 'left' ? 'cg-notify-message-left' : '', " + 
+			"$position === 'right' ? 'cg-notify-message-right' : '']\" " +
+			"ng-style=\"{'margin-left': $centerMargin}\"> " + 
+			"	<h5>{{$classes[0] | uppercase | translate}}</h5>" + 
+			"	<div ng-show=\"!$messageTemplate\">" + 
+			"	{{$message|translate}}" +
+			"	</div>" + 
+			"</div>";
+
+		var service = {};
+		service.error = function(error, position){
+			$log.error(error);
+			notify({
+				startTop: 40, // to not get above the banner
+				duration: 20000,
+				template: template,
+				position: position || 'center',
+				message:'ERR_' + error.cause,
+				classes:['error']
+			});
+		}
+
+		return service;
+	}]);
+})();
