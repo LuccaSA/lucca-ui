c81dd148b8e2ed58489671298fdd74ce957d4a90
lucien <me@lucienbertin.com>
2015-09-07 11:50:51 +0200

    test u [add] even moar tests on luid-moment min and max attribute

--- tests/spec/directives/moment-picker.spec.js
+++ tests/spec/directives/moment-picker.spec.js
@@ -97,7 +97,7 @@ describe('luidMoment', function(){
 			$scope.$digest();
 			isolateScope = elt.isolateScope();
 		});
-		it('should update test.value when incr/decr hours or mins changes', function(){
+		it('should update test.value when incr/decr hours or min changes', function(){
 			$scope.test.min = moment().startOf('day').add(8,'hours').add(1,'minutes');
 			$scope.test.value = moment().startOf('day').add(8,'hours').add(40,'minutes');
 			$scope.$digest();
@@ -131,6 +131,129 @@ describe('luidMoment', function(){
 			$scope.$digest();
 			isolateScope.decrMins();
 			expect($scope.test.value).toEqual($scope.test.min);
+
+			$scope.test.value = moment().startOf('day').add(8,'hours');
+			$scope.$digest();
+			isolateScope.incrMins();
+			expect($scope.test.value).toEqual($scope.test.min);
+		});
+	});
+	describe('with a min=00:00', function(){
+		beforeEach(function(){
+			var tpl = angular.element('<luid-moment ng-model="test.value" min="\'00:00\'"></luid-moment>');
+			elt = $compile(tpl)($scope);
+			$scope.$digest();
+			isolateScope = elt.isolateScope();
+		});
+		it('should limit to today.startOf(day)', function(){
+			$scope.test.value = moment().startOf('day').add(2,'hours').add(40,'minutes');
+			$scope.$digest();
+			isolateScope.decrHours();
+			isolateScope.decrHours();
+			isolateScope.decrHours();
+			isolateScope.decrHours();
+			isolateScope.decrHours();
+			isolateScope.decrHours();
+			expect($scope.test.value.format('LLL')).toEqual(moment().startOf('day').format('LLL'));
+		});
+		it('should be invalid.min if test.value < today', function(){
+			$scope.test.value = moment().startOf('day').add(-1,'day');
+			$scope.$digest();
+			expect(isolateScope.ngModelCtrl.$error.min).toBe(true);
+		});
+		it('should go to  if trying to decr from test.value when test.valiue < test.min', function(){
+			$scope.test.value = moment().startOf('day').add(-1,'day');
+			$scope.$digest();
+			isolateScope.decrMins();
+			expect($scope.test.value.format('LLL')).toEqual(moment().startOf('day').format('LLL'));
+
+			$scope.test.value = moment().startOf('day').add(-1,'day');
+			$scope.$digest();
+			isolateScope.incrMins();
+			expect($scope.test.value.format('LLL')).toEqual(moment().startOf('day').format('LLL'));
+		});
+	});
+	describe('with a max', function(){
+		beforeEach(function(){
+			var tpl = angular.element('<luid-moment ng-model="test.value" max="test.max"></luid-moment>');
+			elt = $compile(tpl)($scope);
+			$scope.$digest();
+			isolateScope = elt.isolateScope();
+		});
+		it('should update test.value when incr/decr hours or max changes', function(){
+			$scope.test.max = moment().startOf('day').add(16,'hours').add(14,'minutes');
+			$scope.test.value = moment().startOf('day').add(15,'hours').add(40,'minutes');
+			$scope.$digest();
+			isolateScope.incrHours();
+			expect($scope.test.value.hours()).toEqual(16);
+			expect($scope.test.value.minutes()).toEqual(14);
+
+			$scope.test.value = moment().startOf('day').add(16,'hours').add(8,'minutes');
+			$scope.$digest();
+			isolateScope.incrMins();
+			expect($scope.test.value.minutes()).toEqual(10);
+			isolateScope.incrMins();
+			expect($scope.test.value.minutes()).toEqual(14);
+			isolateScope.incrMins();
+			expect($scope.test.value.minutes()).toEqual(14);
+			$scope.test.max.add(3, 'minutes');
+			$scope.$digest();
+			isolateScope.incrMins(); 
+			expect($scope.test.value.minutes()).toEqual(15);
+			isolateScope.incrMins();
+			expect($scope.test.value.minutes()).toEqual(17);
+		});
+		it('should be invalid.max if test.value > test.max', function(){
+			$scope.test.max = moment().startOf('day').add(14,'hours');
+			$scope.test.value = moment().startOf('day').add(15,'hours');
+			$scope.$digest();
+			expect(isolateScope.ngModelCtrl.$error.max).toBe(true);
+		});
+		it('should go to test.max if trying to incr from test.value when test.valiue < test.max', function(){
+			$scope.test.max = moment().startOf('day').add(14,'hours');
+			$scope.test.value = moment().startOf('day').add(15,'hours');
+			$scope.$digest();
+			isolateScope.decrMins();
+			expect($scope.test.value).toEqual($scope.test.max);
+			$scope.test.value = moment().startOf('day').add(15,'hours');
+			$scope.$digest();
+			isolateScope.incrMins();
+			expect($scope.test.value).toEqual($scope.test.max);
+		});
+	});
+	describe('with a max=00:00', function(){
+		beforeEach(function(){
+			var tpl = angular.element('<luid-moment ng-model="test.value" max="\'00:00\'"></luid-moment>');
+			elt = $compile(tpl)($scope);
+			$scope.$digest();
+			isolateScope = elt.isolateScope();
+		});
+		it('should limit to today.startOf(day)', function(){
+			$scope.test.value = moment().startOf('day').add(18,'hours').add(40,'minutes');
+			$scope.$digest();
+			isolateScope.incrHours();
+			isolateScope.incrHours();
+			isolateScope.incrHours();
+			isolateScope.incrHours();
+			isolateScope.incrHours();
+			isolateScope.incrHours();
+			expect($scope.test.value.format('LLL')).toEqual(moment().add(1,'day').startOf('day').format('LLL'));
+		});
+		it('should be invalid.min if test.value < today', function(){
+			$scope.test.value = moment().add(1,'day');
+			$scope.$digest();
+			expect(isolateScope.ngModelCtrl.$error.max).toBe(true);
+		});
+		it('should go to  if trying to decr from test.value when test.valiue < test.min', function(){
+			$scope.test.value = moment().add(1,'day');
+			$scope.$digest();
+			isolateScope.decrMins();
+			expect($scope.test.value.format('LLL')).toEqual(moment().add(1,'day').startOf('day').format('LLL'));
+
+			$scope.test.value = moment().startOf('day').add(1,'day');
+			$scope.$digest();
+			isolateScope.incrMins();
+			expect($scope.test.value.format('LLL')).toEqual(moment().add(1,'day').startOf('day').format('LLL'));
 		});
 	});
 });
