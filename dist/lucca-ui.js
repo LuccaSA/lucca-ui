8b3c1ed7ca509c0b4e94b3735706b52d68313dc8
lucien <me@lucienbertin.com>
2015-09-22 17:35:40 +0200

    [transpilation] js

--- dist/lucca-ui.js
+++ dist/lucca-ui.js
@@ -118,7 +118,6 @@
 		return{
 			require:['luidMoment','^ngModel'],
 			controller:'luidMomentController',
-			replace:true,
 			scope: {
 				min:'=', // a moment or a str to specify the min for this input
 				max:'=', // idem for max
