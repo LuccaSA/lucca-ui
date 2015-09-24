6390f8c85d42afe6b5f8f7c6210884a16bfab051
lucien <me@lucienbertin.com>
2015-09-24 16:34:17 +0200

    [transpilation] js after pr #22

--- dist/lucca-ui.js
+++ dist/lucca-ui.js
@@ -47,6 +47,8 @@
 				placeholder: '@',
 				displayProp:'@',
 
+				ngDisabled:'=',
+
 				classes:'@', 
 
 				onSelect:'&',
@@ -54,7 +56,7 @@
 			},
 			restrict: 'E',
 			template:
-			'<ui-select theme="bootstrap" on-select="onSelect()" on-remove="onRemove()" class="lui nguibs-ui-select {{classes}}">' + 
+			'<ui-select theme="bootstrap" on-select="onSelect()" on-remove="onRemove()" class="lui nguibs-ui-select {{classes}}" ng-disabled="ngDisabled">' + 
 			'	<ui-select-match placeholder="{{placeholder}}">{{$select.selected[displayProp]}}</ui-select-match>' + 
 			'	<ui-select-choices repeat="option in options | filter: $select.search">' + 
 			'		<div ng-bind-html="option[displayProp] | highlight: $select.search"></div>' + 
