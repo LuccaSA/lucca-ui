91910a7dba04d2a54079dc0c8aa55ae1078ede69
lucien <me@lucienbertin.com>
2015-09-24 14:31:35 +0200

    luid-select [add] attr ng-disabled

--- js/directives/luid-select.js
+++ js/directives/luid-select.js
@@ -9,6 +9,8 @@
 				placeholder: '@',
 				displayProp:'@',
 
+				ngDisabled:'=',
+
 				classes:'@', 
 
 				onSelect:'&',
@@ -16,7 +18,7 @@
 			},
 			restrict: 'E',
 			template:
-			'<ui-select theme="bootstrap" on-select="onSelect()" on-remove="onRemove()" class="lui nguibs-ui-select {{classes}}">' + 
+			'<ui-select theme="bootstrap" on-select="onSelect()" on-remove="onRemove()" class="lui nguibs-ui-select {{classes}}" ng-disabled="ngDisabled">' + 
 			'	<ui-select-match placeholder="{{placeholder}}">{{$select.selected[displayProp]}}</ui-select-match>' + 
 			'	<ui-select-choices repeat="option in options | filter: $select.search">' + 
 			'		<div ng-bind-html="option[displayProp] | highlight: $select.search"></div>' + 
