eb05c27b4a24043d3518108abeb06eaade8d4268
lucien <me@lucienbertin.com>
2015-09-25 13:22:50 +0200

    demo [add] trying to merge with the version availabel om branch gh-pages

---
+++ demo/js/demo.js
@@ -0,0 +1,25 @@
+(function(){
+	'use strict';
+	var colors = ['none', 'primary', 'secondary', 'success', 'warning', 'error', 'grey', 'light', 'yellow', 'green', 'orange', 'red'];
+	
+	// we use underscore cuz it's awesome
+	// http://underscorejs.org/
+	angular.module('underscore', []).factory('_', function () { return window._; });
+
+	angular.module('demoApp',['ui.bootstrap']);
+
+	angular.module('demoApp')
+	.controller('buttonsCtrl', ['$scope', function($scope){
+		$scope.colors = colors;
+		$scope.styles = ['default', 'flat', 'wired', 'filling'];
+		$scope.sizes = ['small', 'default', 'large', 'x-large'];
+
+		$scope.color = '';
+		$scope.style = '';
+		$scope.size = '';
+		$scope.status = '';
+		$scope.inverted = false;
+		$scope.disabled = false;
+
+	}]);
+})();
