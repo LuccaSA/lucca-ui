40dcec9c266015b76f2818bcef9ab8a8a2a8870c
lucien <me@lucienbertin.com>
2015-09-25 13:07:35 +0200

    demo [add] filters.html page to demonstrate the available filters in lucca-ui

--- demo/js/filters.js
+++ demo/js/filters.js
@@ -31,6 +31,32 @@
 			$scope.myValue = moment().startOf('week');
 		}
 	}]);
+	angular.module('filterApp')
+	.controller('calendarCtrl', ['$scope', 'moment', function($scope, moment){
+		$scope.myValue = moment().add(-1,'days');
+		$scope.myRefDate = moment().add(-2,'days');
+		$scope.setToThisWeek = function(){
+			$scope.myValue = moment().startOf('week');
+		};
+		$scope.setToLastWeek = function(){
+			$scope.myValue = moment().add(-1,'week');
+		};
+	}]);
+
+	angular.module('filterApp')
+	.controller('friendlyrangeCtrl', ['$scope', 'moment', function($scope, moment){
+		$scope.myBlock = { start: moment().startOf('month'), end: moment().add(1,'month').startOf('month') };
+	}]);
+
+	angular.module('filterApp')
+	.controller('durationCtrl', ['$scope', 'moment', function($scope, moment){
+		$scope.myValue = moment.duration("10:00:00");
+	}]);
+
+	angular.module('filterApp')
+	.controller('humanizeCtrl', ['$scope', 'moment', function($scope, moment){
+		$scope.myValue = moment.duration("10:00:00");
+	}]);
 
 
 })();
