e57c56db39b19d64eae56174653a50988badfc05
lucien <me@lucienbertin.com>
2015-09-29 15:15:04 +0200

    filters-testu [add] luifNumber

--- tests/spec/genericFilters.spec.js
+++ tests/spec/genericFilters.spec.js
@@ -3,7 +3,7 @@ describe('luif.timefilters', function(){
 	beforeEach(module('lui'));
 	beforeEach(module('lui.filters'));
 
-	var $filter, moment;
+	var $filter;
 	beforeEach(inject(function (_$filter_) {
 		$filter = _$filter_;
 	}));
@@ -40,6 +40,17 @@ describe('luif.timefilters', function(){
 		it('should work', function(){
 			expect(luifStartFrom('abc')).toEqual('abc');
 			expect(luifStartFrom('abc',1)).toEqual('bc');
+		});
+	});
+	describe('luifNumber', function(){
+		var luifNumber;
+		beforeEach(function(){
+			luifNumber = $filter('luifNumber');
+		});
+		it('should work', function(){
+			expect(luifNumber(10.5).$$unwrapTrustedValue()).toEqual($filter("number")(10.5,2));
+			expect(luifNumber(10.5,3).$$unwrapTrustedValue()).toEqual($filter("number")(10.5,3));
+			expect(luifNumber(10,1).$$unwrapTrustedValue()).toEqual("10<span style=\"opacity:0\">.0</span>");
 		});
 	});
 });
