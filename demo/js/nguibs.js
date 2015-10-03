909f67ccfc8706c34d66258428afaf95b1dd5b39
bpaugam <bpaugam@gmail.com>
2015-10-03 12:21:01 +0200

    Demos [ADD] Added a nguibs demo

---
+++ demo/js/nguibs.js
@@ -0,0 +1,26 @@
+
+(function(){
+	angular.module('nguibsApp',['lui', 'ngSanitize', 'ui.bootstrap', 'ui.select']);
+
+	angular.module('nguibsApp')
+	.controller('uiselectCtrl', ['$scope', function($scope){
+		$scope.person = {};
+		$scope.people = [
+			{ name: 'Adam',      email: 'adam@email.com',      age: 10 },
+			{ name: 'Amalie',    email: 'amalie@email.com',    age: 12 },
+			{ name: 'Wladimir',  email: 'wladimir@email.com',  age: 30 },
+			{ name: 'Samantha',  email: 'samantha@email.com',  age: 31 },
+			{ name: 'Estefanía', email: 'estefanía@email.com', age: 16 },
+			{ name: 'Natasha',   email: 'natasha@email.com',   age: 54 },
+			{ name: 'Nicole',    email: 'nicole@email.com',    age: 43 },
+			{ name: 'Adrian',    email: 'adrian@email.com',    age: 21 }
+		];
+
+		$scope.availableColors = ['Red','Green','Blue','Yellow','Magenta','Maroon','Umbra','Turquoise'];
+		$scope.colors = ['Blue','Red'];
+	}]);
+
+	angular.module('nguibsApp')
+	.controller('tooltipCtrl', ['$scope', function($scope){
+	}]);
+})();
