997ed41a6679c317656e233ccf4a7a7e82848fbf
Benoit PAUGAM <bpaugam@lucca.fr>
2015-10-05 13:50:46 +0200

    Inputs [ENH] Refactoring

--- js/directives/moment-picker.js
+++ js/directives/moment-picker.js
@@ -18,11 +18,11 @@
 						scope.mins = momentValue.format('mm');
 					}
 				};
-				ngModelCtrl.setValue = function(newMomentValue){ 
+				ngModelCtrl.setValue = function(newMomentValue){
 					if(!newMomentValue){
 						ngModelCtrl.$setViewValue(undefined);
 					}else{
-						ngModelCtrl.$setViewValue(newMomentValue.format(format)); 
+						ngModelCtrl.$setViewValue(newMomentValue.format(format));
 					}
 				};
 			}else{
@@ -65,7 +65,7 @@
 				format:'=', // alows ng-model to be a string with the right format
 
 				// hacks
-				minOffset:'=', // to avoid having to say min=val1+val2 because it causes an other digest cycle, we give the offset and the 
+				minOffset:'=', // to avoid having to say min=val1+val2 because it causes an other digest cycle, we give the offset and the
 				maxOffset:'='
 			},
 			templateUrl:"lui/directives/luidMoment.html",
@@ -110,7 +110,7 @@
 			}
 
 			newValue.seconds(0);
-			// update 
+			// update
 			update(newValue);
 		};
 		var update = function(newValue){
@@ -135,10 +135,10 @@
 		// string value changed
 		$scope.changeHours = function(){
 			// if hours does not satisfy the pattern [0-9]{0,2}
-			if($scope.hours === undefined){ 
+			if($scope.hours === undefined){
 				$scope.ngModelCtrl.$setValidity('pattern', false);
 				return update(undefined);
-			} 
+			}
 			$scope.ngModelCtrl.$setValidity('pattern', true);
 
 			if($scope.hours === ""){
@@ -155,10 +155,10 @@
 			updateWithoutRender(getInputedTime());
 		};
 		$scope.changeMins = function(){
-			if($scope.mins === undefined){ 
+			if($scope.mins === undefined){
 				$scope.ngModelCtrl.$setValidity('pattern', false);
 				return update(undefined);
-			} 
+			}
 			$scope.ngModelCtrl.$setValidity('pattern', true);
 
 			updateWithoutRender(getInputedTime());
@@ -277,7 +277,7 @@
 			if(!$scope.min){ return undefined; } // min attr not specified
 			if(!!$scope.min.isValid && !!$scope.min.isValid()){ // check if min is a valid moment
 				min = moment($scope.min);
-			}else if(moment($scope.min,'YYYY-MM-DD HH:mm').isValid()){ // check if min is parsable by moment 
+			}else if(moment($scope.min,'YYYY-MM-DD HH:mm').isValid()){ // check if min is parsable by moment
 				min = moment($scope.min,'YYYY-MM-DD HH:mm');
 			}else if(moment($scope.min, 'HH:mm').isValid()){ // check if min is leke '23:15'
 				var refDate = getRefDate();
@@ -293,7 +293,7 @@
 			if(!$scope.max){ return undefined; } // max attr not specified
 			if(!!$scope.max.isValid && !!$scope.max.isValid()){ // check if max is a valid moment
 				max = moment($scope.max);
-			}else if(moment($scope.max,'YYYY-MM-DD HH:mm').isValid()){ // check if max is parsable by moment 
+			}else if(moment($scope.max,'YYYY-MM-DD HH:mm').isValid()){ // check if max is parsable by moment
 				max = moment($scope.max,'YYYY-MM-DD HH:mm');
 			}else if(moment($scope.max, 'HH:mm').isValid()){ // check if max is leke '23:15'
 				var refDate = getRefDate();
@@ -394,8 +394,8 @@
 	}]);
 	angular.module("lui.templates.momentpicker").run(["$templateCache", function($templateCache) {
 		$templateCache.put("lui/directives/luidMoment.html",
-			"<div class='luid-moment' ng-class='{disabled:disabled}'>" + 
-			"	<input type='text' ng-model='hours' ng-change='changeHours()' luid-select-on-click ng-pattern='pattern' luid-focus-on='focusHours'   ng-focus='focusHours()' ng-blur='blurHours()' ng-disabled='disabled' maxlength=2>:" +
+			"<div class='luid-moment' ng-class='{disabled:disabled}'>" +
+			"	<input type='text' ng-model='hours' ng-change='changeHours()' luid-select-on-click ng-pattern='pattern' luid-focus-on='focusHours'   ng-focus='focusHours()' ng-blur='blurHours()' ng-disabled='disabled' maxlength=2> : " +
 			// This indentation issue is normal and needed
 			"	<input type='text' ng-model='mins'  ng-change='changeMins()'  luid-select-on-click ng-pattern='pattern' luid-focus-on='focusMinutes' ng-focus='focusMins()'  ng-blur='blurMins()'  ng-disabled='disabled' maxlength=2>" +
 			"	<i ng-if='hasButtons' ng-click='incrHours()' ng-show='showButtons||hoursFocused||minsFocused' class='lui mp-button top left north arrow icon'     ng-class='{disabled:maxed}'></i>" +
