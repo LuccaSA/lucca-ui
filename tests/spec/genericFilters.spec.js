1af0f1526dff4238b29285540a954be78281073a
lucien <me@lucienbertin.com>
2015-10-13 18:39:47 +0200

    luifNumber [enh] issue #35, added a placeholder arg to use when the _input is undefined, enhanced comportment to always return integralPart<span>separator and decimalPart</span> to allow for selecting decimal part via css selector and appluy custom style, also added support for non numeric inputs

--- tests/spec/genericFilters.spec.js
+++ tests/spec/genericFilters.spec.js
@@ -47,10 +47,32 @@ describe('luif.timefilters', function(){
 		beforeEach(function(){
 			luifNumber = $filter('luifNumber');
 		});
-		it('should work', function(){
-			expect(luifNumber(10.5).$$unwrapTrustedValue()).toEqual($filter("number")(10.5,2));
-			expect(luifNumber(10.5,3).$$unwrapTrustedValue()).toEqual($filter("number")(10.5,3));
+		it('should work with numeric values', function(){
+			expect(luifNumber(10.5).$$unwrapTrustedValue()).toEqual("10<span>.50</span>");
+			expect(luifNumber(10.5,3).$$unwrapTrustedValue()).toEqual("10<span>.500</span>");
 			expect(luifNumber(10,1).$$unwrapTrustedValue()).toEqual("10<span style=\"opacity:0\">.0</span>");
+			expect(luifNumber(undefined,undefined,undefined).$$unwrapTrustedValue()).toEqual("<span style=\"opacity:0\">.00</span>");
+			expect(luifNumber(undefined,undefined,0).$$unwrapTrustedValue()).toEqual("0<span style=\"opacity:0\">.00</span>");
+			expect(luifNumber(undefined,undefined,'nothing').$$unwrapTrustedValue()).toEqual("nothing<span style=\"opacity:0\">.00</span>");
+			expect(luifNumber(undefined,4,undefined).$$unwrapTrustedValue()).toEqual("<span style=\"opacity:0\">.0000</span>");
+			expect(luifNumber(undefined,4,0).$$unwrapTrustedValue()).toEqual("0<span style=\"opacity:0\">.0000</span>");
+			expect(luifNumber(undefined,4,'nothing').$$unwrapTrustedValue()).toEqual("nothing<span style=\"opacity:0\">.0000</span>");
+		});
+		it('should add a margin right to string values', function(){
+			expect(luifNumber('a string').$$unwrapTrustedValue()).toEqual("a string<span style=\"opacity:0\">.00</span>");
+			expect(luifNumber('a string',4).$$unwrapTrustedValue()).toEqual("a string<span style=\"opacity:0\">.0000</span>");
+		});
+		it('should work with precision = 0', function(){
+			expect(luifNumber(12.4,0).$$unwrapTrustedValue()).toEqual("12<span style=\"opacity:0\"></span>");
+			expect(luifNumber('a string',0).$$unwrapTrustedValue()).toEqual("a string<span style=\"opacity:0\"></span>");
+		});
+		it('should work with value = undefined', function(){
+			expect(luifNumber(undefined).$$unwrapTrustedValue()).toEqual("<span style=\"opacity:0\">.00</span>");
+			expect(luifNumber(undefined,undefined,0).$$unwrapTrustedValue()).toEqual("0<span style=\"opacity:0\">.00</span>");
+			expect(luifNumber(undefined,undefined,'nothing').$$unwrapTrustedValue()).toEqual("nothing<span style=\"opacity:0\">.00</span>");
+			expect(luifNumber(undefined,4).$$unwrapTrustedValue()).toEqual("<span style=\"opacity:0\">.0000</span>");
+			expect(luifNumber(undefined,4,0).$$unwrapTrustedValue()).toEqual("0<span style=\"opacity:0\">.0000</span>");
+			expect(luifNumber(undefined,4,'nothing').$$unwrapTrustedValue()).toEqual("nothing<span style=\"opacity:0\">.0000</span>");
 		});
 	});
 });
