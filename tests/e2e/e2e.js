f39d25b00e7c2f990a081fb0e29712216134208e
lucien <me@lucienbertin.com>
2015-10-19 16:34:39 +0200

    e2e [add] check dis out broh, luifPlaceholder is tested with protractor homie!

--- tests/e2e/e2e.js
+++ tests/e2e/e2e.js
@@ -2,4 +2,10 @@
 	'use strict';
 
 	angular.module('e2eApp', ['lui']);
+
+	angular.module('e2eApp')
+	.controller("luifPlaceholderCtrl",['$scope', function($scope){
+		$scope.myValue = "stuff";
+		$scope.myPlaceholder = "placeholder";
+	}]);
 })();
