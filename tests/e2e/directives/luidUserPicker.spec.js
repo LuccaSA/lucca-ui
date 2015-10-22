a693bb227920e356a0748294211176c5576723d7
pierop <apieropan@lucca.fr>
2015-10-22 09:42:31 +0200

    tests e2e [add] with former employees

--- tests/e2e/directives/luidUserPicker.spec.js
+++ tests/e2e/directives/luidUserPicker.spec.js
@@ -1,18 +1,28 @@
 describe('luidUserPicker', function() {
 	var myUserDiv;
 	var myUserPicker;
+	var myUserPickerFormerEmployees;
 	var myUserPickerInput;
+	var myUserPickerInputFormerEmployees;
 	var myUserPickerChoices;
+	var myUserPickerChoicesFormerEmployees;
+	var myUserPickerContainer;
 
 	beforeEach(function() {
-		myUserPicker = element(by.id("luidUserPicker_myUser_select"));
-		myUserPickerInput = myUserPicker.all(by.tagName("input")).first();
-		myUserPickerContainer = myUserPicker.element(by.className("ui-select-container"));
-		myUserPickerChoicesGroup = myUserPicker.element(by.className("ui-select-choices-group"));
+		myUserDiv = element(by.id('luidUserPicker_myUser_value'));
+		myUserPicker = element(by.id('luidUserPicker_myUser_select'));
+		myUserPickerInput = myUserPicker.all(by.tagName('input')).first();
+		myUserPickerContainer = myUserPicker.element(by.className('ui-select-container'));
 		myUserPickerChoices = myUserPicker.all(by.className('ui-select-choices-row'));
-		myUserDiv = element(by.id("luidUserPicker_myUser_value"));
+		/* luid-user-picker with former employees */
+		myUserPickerFormerEmployees = element(by.id('luidUserPicker_myUser_select_former_employees'));
+		myUserPickerChoicesFormerEmployees = myUserPickerFormerEmployees.all(by.className('ui-select-choices-row'));
+		myUserPickerInputFormerEmployees = myUserPickerFormerEmployees.all(by.tagName('input')).first();
 	});
 
+	/*****************/
+	/***** BASIC *****/
+	/*****************/
 	it("should display init input value", function() {
 		myUserPicker.getText().then(function(text){
 			expect(text).toBe('Lucien Bertin');
@@ -23,15 +33,14 @@ describe('luidUserPicker', function() {
 		expect(myUserPickerContainer.getAttribute('class')).toMatch('open');
 		expect(myUserPickerChoices.count()).toBe(6);
 	});
-	it("should disable last choice in dropdown menu", function() {
+	it("should disable sixth choice in dropdown menu because it is the overflow message", function() {
 		expect(myUserPickerContainer.getAttribute('class')).toMatch('open');
 		var lastChoice = myUserPickerChoices.get(5).getWebElement();
 		expect(lastChoice.getAttribute('class')).toMatch('disabled');
 	});
-	it('should update the selected user', function() {
+	it('should update the selected user and close the dropdown menu on click on the choice', function() {
 		myUserPickerChoices.get(1).getWebElement().click();
-		//myUserPickerInput.sendKeys(protractor.Key.DOWN);
-		//myUserPickerInput.sendKeys(protractor.Key.ENTER);
+		expect(myUserPickerContainer.getAttribute('class')).not.toMatch('open');
 		myUserPicker.getText().then(function(userPickerText) {
 			// Check that the text in the input is the name of the selected user
 			expect(userPickerText).toBe('Elsa Arrou-Vignod');
@@ -47,6 +56,10 @@ describe('luidUserPicker', function() {
 		expect(myUserPickerContainer.getAttribute('class')).toMatch('open');
 		expect(myUserPickerChoices.count()).toBe(2);
 	});
+
+	/********************/
+	/***** HOMONYMS *****/
+	/********************/
 	it('should display homonyms', function() {
 		myUserPickerInput.clear();
 		myUserPickerInput.sendKeys("a");
@@ -55,5 +68,27 @@ describe('luidUserPicker', function() {
 		// Check that the first user is displayed as 'homonym' (it has 2 'small' tags as children)
 		var smallTags = myUserPickerChoices.get(0).all(by.tagName('small'));
 		expect(smallTags.count()).toBe(2);
+		// In order to close the dropdown menu
+		myUserPickerChoices.get(0).getWebElement().click();
+	});
+
+	/****************************/
+	/***** FORMER EMPLOYEES *****/
+	/****************************/
+	it('should display former employees', function() {
+		myUserPickerFormerEmployees.click();
+		// Check that the last user in the dropdown menu is displayed as 'former employee' (it has a 'small' tag as child)
+		var smallTag = myUserPickerChoicesFormerEmployees.get(4).all(by.tagName('small'));
+		expect(smallTag.count()).toBe(1);
+	});
+
+	/***************************************/
+	/***** FORMER EMPLOYEES + HOMONYMS *****/
+	/***************************************/
+	it('should display former employees and homonyms', function() {
+		myUserPickerInputFormerEmployees.sendKeys("a");
+		// Check that the last user in the dropdown menu is displayed as 'former employee' and 'homonym' (it has 3 'small' tags as children)
+		var smallTag = myUserPickerChoicesFormerEmployees.get(1).all(by.tagName('small'));
+		expect(smallTag.count()).toBe(3);
 	});
 });
--- tests/e2e/e2e.js
+++ tests/e2e/e2e.js
@@ -23,7 +23,7 @@
 	angular.module('e2eApp')
 	.controller("luidUserPickerCtrl", ['$scope', '$httpBackend', '_', function($scope, $httpBackend, _) {
 
-		/****** Http calls mocking ************/
+		/***** Http calls mocking *****/
 		var users = [{"id":1,"firstName":"Guillaume","lastName":"Allain"},{"id":2,"firstName":"Elsa","lastName":"Arrou-Vignod"},{"id":3,"firstName":"Chloé","lastName":"Azibert Yekdah"},{"id":4,"firstName":"Clément","lastName":"Barbotin"},{"id":5,"firstName":"Lucien","lastName":"Bertin"},{"id":6,"firstName":"Jean-Baptiste","lastName":"Beuzelin"},{"id":7,"firstName":"Kevin","lastName":"Brochet"},{"id":8,"firstName":"Alex","lastName":"Carpentieri"},{"id":9,"firstName":"Bruno","lastName":"Catteau"},{"id":10,"firstName":"Orion","lastName":"Charlier"},{"id":11,"firstName":"Sandrine","lastName":"Conraux"},{"id":12,"firstName":"Tristan","lastName":"Couëtoux du Tertre"},{"id":13,"firstName":"Patrick","lastName":"Dai"},{"id":14,"firstName":"Larissa","lastName":"De Andrade Gaulia"},{"id":15,"firstName":"Christophe","lastName":"Demarle"},{"id":16,"firstName":"Manon","lastName":"Desbordes"},{"id":17,"firstName":"Nicolas","lastName":"Faugout"},{"id":18,"firstName":"Brice","lastName":"Francois"},{"id":19,"firstName":"Tristan","lastName":"Goguillot"},{"id":20,"firstName":"Julia","lastName":"Ivanets"}];
 		var users_FE = [{"id":1,"firstName":"Guillaume","lastName":"Allain","dtContractEnd":null},{"id":2,"firstName":"Elsa","lastName":"Arrou-Vignod","dtContractEnd":null},{"id":3,"firstName":"Chloé","lastName":"Azibert Yekdah","dtContractEnd":null},{"id":4,"firstName":"Clément","lastName":"Barbotin","dtContractEnd":null},{"id":5,"firstName":"Saâd","lastName":"Benmansour","dtContractEnd":"2011-09-30T00:00:00"},{"id":6,"firstName":"ALEXANDRE","lastName":"BERTHON","dtContractEnd":"2015-08-07T00:00:00"},{"id":7,"firstName":"Lucien","lastName":"Bertin","dtContractEnd":null},{"id":8,"firstName":"Jean-Baptiste","lastName":"Beuzelin","dtContractEnd":null},{"id":9,"firstName":"Anne-Murielle","lastName":"Blanquet","dtContractEnd":"2015-04-30T00:00:00"},{"id":10,"firstName":"Estelle","lastName":"Bonet","dtContractEnd":"2012-04-30T00:00:00"},{"id":11,"firstName":"Aurélien","lastName":"Bottazini","dtContractEnd":"2007-03-31T00:00:00"},{"id":12,"firstName":"Kevin","lastName":"Brochet","dtContractEnd":"2017-08-31T00:00:00"},{"id":13,"firstName":"XAVIER","lastName":"CAMPENON","dtContractEnd":"2015-04-30T00:00:00"},{"id":14,"firstName":"Alex","lastName":"Carpentieri","dtContractEnd":null},{"id":15,"firstName":"Bruno","lastName":"Catteau","dtContractEnd":null},{"id":16,"firstName":"Onur","lastName":"Celebi","dtContractEnd":"2009-06-30T00:00:00"},{"id":17,"firstName":"Orion","lastName":"Charlier","dtContractEnd":null},{"id":18,"firstName":"ALAN","lastName":"CHAUCHET","dtContractEnd":"2015-09-01T00:00:00"},{"id":19,"firstName":"Sandrine","lastName":"Conraux","dtContractEnd":null},{"id":20,"firstName":"Rémy","lastName":"Coudercher","dtContractEnd":"2012-08-10T00:00:00"}];
 		var homonyms = [{"id":450,"firstName":"Lucien","lastName":"Bertin"},{"id":421,"firstName":"Lucien","lastName":"Bertin"},{"id":401,"firstName":"Jean-Baptiste","lastName":"Beuzelin"},{"id":416,"firstName":"Benoit","lastName":"Paugam"}];
@@ -51,23 +51,33 @@
 		var RESPONSE_homonyms_properties_FE = {header:{},data:{items:[{id:421,firstName:"Lucien",lastName:"Bertin",department:{name:"BU Timmi/Lucca"},legalEntity:{name:"Lucca"},employeeNumber:"00068",mail:"no-reply@lucca.fr"},{id:449,firstName:"Lucien",lastName:"Bertin",department:{name:"Sales"},legalEntity:{name:"Lucca"},employeeNumber:"00099",mail:"no-reply@lucca.fr"},{id:450,firstName:"Lucien",lastName:"Bertin",department:{name:"Marketing"},legalEntity:{name:"Lucca"},employeeNumber:"00110",mail:"no-reply@lucca.fr"}]}};
 
 		/********************************/
-		/******     NO HOMONYMS     *****/
+		/******    NO HOMONYMS      *****/
 		/****** NO FORMER EMPLOYEES *****/
 		/********************************/
-		$httpBackend.whenGET(/api\/v3\/users\/find\?formerEmployees=false\&.*/i).respond(200, RESPONSE_20_users_FE);
-		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{1}\&formerEmployees=false\&.*/i).respond(200, RESPONSE_10_users_FE);
-		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{2}\&formerEmployees=false\&.*/i).respond(200, RESPONSE_5_users_FE);
-		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{3}\&formerEmployees=false\&.*/i).respond(200, RESPONSE_2_users_FE);
-		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{4}\&formerEmployees=false\&.*/i).respond(200, RESPONSE_1_users_FE);
+		$httpBackend.whenGET(/api\/v3\/users\/find\?formerEmployees=false\&.*/i).respond(200, RESPONSE_20_users);
+		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{1}\&formerEmployees=false\&.*/i).respond(200, RESPONSE_10_users);
+		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{2}\&formerEmployees=false\&.*/i).respond(200, RESPONSE_5_users);
+		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{3}\&formerEmployees=false\&.*/i).respond(200, RESPONSE_2_users);
+		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{4}\&formerEmployees=false\&.*/i).respond(200, RESPONSE_1_users);
 		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{5,}\&formerEmployees=false\&.*/i).respond(200, RESPONSE_0_users);
+
+		/*****************************/
+		/******    NO HOMONYMS   *****/
+		/****** FORMER EMPLOYEES *****/
+		/*****************************/
+		$httpBackend.whenGET(/api\/v3\/users\/find\?formerEmployees=true\&.*/i).respond(200, RESPONSE_20_users_FE);
+		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{1}\&formerEmployees=true\&.*/i).respond(200, RESPONSE_10_users_FE);
+		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{2}\&formerEmployees=true\&.*/i).respond(200, RESPONSE_5_users_FE);
+		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{3}\&formerEmployees=true\&.*/i).respond(200, RESPONSE_2_users_FE);
+		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{4}\&formerEmployees=true\&.*/i).respond(200, RESPONSE_1_users_FE);
+		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=[b-z]{5,}\&formerEmployees=true\&.*/i).respond(200, RESPONSE_0_users);
+	
 		/*******************************/
 		/******      HOMONYMS      *****/
 		/***** NO FORMER EMPLOYEES *****/
 		/*******************************/
 		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=a\&formerEmployees=false\&.*/i).respond(200, RESPONSE_4_users_homonyms);
-
 		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=a.\&formerEmployees=false\&.*/i).respond(200, RESPONSE_2_users_homonyms);
-
 		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=a.*\&formerEmployees=false\&.*/i).respond(200, RESPONSE_0_users);
 
 		/****************************/
@@ -80,16 +90,13 @@
 		/***** FORMER EMPLOYEES *****/
 		/****************************/
 		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=a\&formerEmployees=true\&.*/i).respond(200, RESPONSE_5_users_homonyms_FE);
-
 		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=a.\&formerEmployees=true\&.*/i).respond(200, RESPONSE_3_users_homonyms_FE);
-
 		$httpBackend.whenGET(/api\/v3\/users\/find\?clue=a.*\&formerEmployees=true\&.*/i).respond(200, RESPONSE_0_users);
 
 		/****************************/
 		/***** HOMONYMS DETAILS *****/
 		/****************************/
 		$httpBackend.whenGET(/api\/v3\/users\?id=450,449,421.*/i).respond(200, RESPONSE_homonyms_properties_FE);
-
 
 		$scope.myUser = {};
 		$scope.myUser.selected = { firstName: 'Lucien', lastName: 'Bertin' };
