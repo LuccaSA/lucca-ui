5192ee8622c37cfd9631603846444145bb512c47
pierop <apieropan@lucca.fr>
2015-10-23 14:38:43 +0200

    demo [ref] added routing in demo.js

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,34 +1,76 @@
 (function(){
 	'use strict';
-	var colors = ['none', 'primary', 'secondary', 'success', 'warning', 'error', 'grey', 'light', 'yellow', 'green', 'orange', 'red'];
-	
+
 	// we use underscore cuz it's awesome
 	// http://underscorejs.org/
 	angular.module('underscore', []).factory('_', function () { return window._; });
+	angular.module('moment', []).factory('moment', function () { return window.moment; });
 
-	angular.module('demoApp',['ui.bootstrap', 'ngRoute']);
+	angular.module('demoApp',['lui', 'ui.bootstrap', 'ngRoute', 'ngSanitize', 'ui.select']);
 
 	angular.module('demoApp')
-	.controller('buttonsCtrl', ['$scope', function($scope){
-		$scope.colors = colors;
-		$scope.styles = ['default', 'flat', 'wired', 'filling'];
-		$scope.sizes = ['small', 'default', 'large', 'x-large'];
-
-		$scope.color = '';
-		$scope.style = '';
-		$scope.size = '';
-		$scope.status = '';
-		$scope.inverted = false;
-		$scope.disabled = false;
-
+	.controller('bannerCtrl', ['$scope', '$location', function($scope, $location) {
+		$scope.isActive = function(viewLocation) {
+			return viewLocation === $location.path();
+		};
 	}]);
 
 	angular.module('demoApp')
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
 			.when('/less', {
-				templateUrl: 'less.html',
+				templateUrl: 'less-framework.html',
+			})
+			.when('/icons', {
+				templateUrl: 'icons.html'
+			})
+			.when('/animations', {
+				templateUrl: 'animations.html',
+			})
+			.when('/nguibs', {
+				templateUrl: 'nguibs.html',
+			})
+			.when('/filters', {
+				templateUrl: 'filters.html',
+			})
+			.when('/directives', {
+				templateUrl: 'directives.html',
+			})
+			.when('/lucca', {
+				templateUrl: 'lucca-spe.html',
 			})
 			.otherwise({ redirectTo: '/less'});
+
+		// $urlRouterProvider.otherwise('/less');
+
+		// $stateProvider
+		// .state('less', {
+		// 	url: '/less',
+		// 	templateUrl: 'less-framework.html'
+		// })
+		// .state('icons', {
+		// 	url: '/icons',
+		// 	templateUrl: 'icons.html'
+		// })
+		// .state('animations', {
+		// 	url: '/animations',
+		// 	templateUrl: 'animations.html'
+		// })
+		// .state('nguibs', {
+		// 	url: '/nguibs',
+		// 	templateUrl: 'nguibs.html'
+		// })
+		// .state('filters', {
+		// 	url: '/filters',
+		// 	templateUrl: 'filters.html'
+		// })
+		// .state('directives', {
+		// 	url: '/directives',
+		// 	templateUrl: 'directives.html'
+		// })
+		// .state('luccaSpe', {
+		// 	url: '/lucca',
+		// 	templateUrl: 'lucca-spe.html'
+		// })
 	}]);
 })();
