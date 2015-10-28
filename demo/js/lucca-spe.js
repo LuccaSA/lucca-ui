f4f3014e12b2c74a30dc1dff88d9b0458befdbd2
pierop <apieropan@lucca.fr>
2015-10-28 18:48:41 +0100

    demo/lucca-spe [add] custom-filter + custom-properties + appId + operations features + demo

--- demo/js/lucca-spe.js
+++ demo/js/lucca-spe.js
@@ -7,8 +7,9 @@
 		$scope.apiCalls = [];
 		$scope.myUser = {};
 		$scope.local = "lucca";
+		$scope.authToken;
+		$scope.customFilter = 'hasShortName'; // contains the custom filter selected
 
-		$scope.authToken
 		$scope.auth = function(){
 			$http.post("//" + $scope.local + ".local/auth/userlogin?login=passepartout&password=")
 			.success(function(response){
@@ -38,5 +39,26 @@
 		
 		$httpBackend.whenGET(/\/\/\w*.local\/.*/).passThrough();
 		$httpBackend.whenPOST(/\/\/\w*.local\/.*/).passThrough();
+
+		/* Custom filters */
+		$scope.hasShortName = function(user) {
+			return ((user.lastName.length + user.firstName.length) <= 15);
+		};
+
+		$scope.beginsWithConsonant = function(user) {
+			var vowels = "aeiouy";
+			var beginsWithConsonant = true;
+
+			_.each(vowels, function(vowel) {
+				if (user.firstName[0].toLowerCase() === vowel) {
+					beginsWithConsonant = false;
+				}
+			});
+			return beginsWithConsonant;
+		};
+
+		$scope.nameContainsT = function(user) {
+			return (!_.contains(user.firstName.toLowerCase(), 't') && !_.contains(user.lastName.toLowerCase(), 't'));
+		}
 	}]);
 })();
