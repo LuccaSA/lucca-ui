c58ac6b1c36794a1c8efdc5de1a6a43b670e3d5b
lucien <me@lucienbertin.com>
2015-12-14 11:48:36 +0100

    demo/lucca [add] luid-translations section

--- demo/js/lucca-spe.js
+++ demo/js/lucca-spe.js
@@ -81,5 +81,9 @@
 	angular.module("demoApp")
 	.controller("translationsCtrl", ["$scope", function($scope){
 		$scope.myTrads = {en: "some stuff", fr: "des bidules"};
+		$scope.count = 0
+		$scope.changed = function(){
+			$scope.count++;
+		}
 	}]);
 })();
