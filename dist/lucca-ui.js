6833ddbe3579ce5ef688e632f79de35a2758383c
Alex Carpentieri <alex.carpe@gmail.com>
2016-01-06 14:28:11 +0100

    added tests e2e for lui-day-block

--- dist/lucca-ui.js
+++ dist/lucca-ui.js
@@ -225,16 +225,44 @@
         return {
             template : 
             '<div class="day-bloc">'+
-            '<div class="weekday">{{controller.date | luifMoment: \'dddd\'}}'+
+            
+            '<div ng-style = \'{'+
+            'color: controller.firstColor'+
+            '}\' '+
+            'ng-if = "controller.showDay" class="weekday">{{controller.date | luifMoment: \'dddd\'}}'+
             '</div>'+
-            '<div class="day">{{controller.date | luifMoment:\'DD\'}}</div>'+
-            '<div class="month">{{controller.date | luifMoment: \'MMM\' | limitTo : 3}}'+
+            
+            '<div ng-style = \'{ '+
+            'border: "1px solid " + controller.firstColor, '+
+            '"background-color": controller.firstColor, '+
+            '"color": controller.secondColor '+
+            '}\' class="day">{{controller.date | luifMoment:\'DD\'}}'+
             '</div>'+
-            '<div class="year">{{controller.date | luifMoment: \'YYYY\'}}</div>'+
-            '</div>,',
+            
+            '<div ng-style = \'{'+
+            '"background-color": controller.secondColor, '+
+            ' "border-left" : "1px solid " + controller.firstColor,'+
+            ' "border-right" : "1px solid " + controller.firstColor, '+
+            ' color: controller.firstColor '+
+            '}\' class="month">{{controller.date | luifMoment: \'MMM\' | limitTo : 3}}'+
+            '</div>'+
+            
+            '<div ng-style = \'{'+
+            '"background-color": controller.secondColor, '+
+            ' "border-left" : "1px solid " + controller.firstColor,'+
+            ' "border-right" : "1px solid " + controller.firstColor, '+
+            ' "border-bottom" : "1px solid " + controller.firstColor, '+
+            ' color: controller.firstColor '+
+            '}\'  class="year">{{controller.date | luifMoment: \'YYYY\'}}'+
+            '</div>'+
+            
+            '</div>',
 
             scope : {
-            date: '='
+                date: '=',
+                showDay: '=',
+                firstColor: '=',
+                secondColor: '='
             },
             
             restrict : 'E',
@@ -248,9 +276,7 @@
     angular
     .module('lui.directives')
     .directive('luidDayBlock', DayBlockDirective)
-    .controller('DayBlockController', function(){
-
-    });
+    .controller('DayBlockController', function(){});
     
 })();
 
