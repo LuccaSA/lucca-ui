40a94fb2dfb4aa81c66c1d774cd7c9f0bd7cf7c8
Paul Louis <plouis@lucca.fr>
2016-02-17 17:01:38 +0100

    dip réinstallation

---
+++ ts/table-grid/table-grid.spec.ts
@@ -0,0 +1,14 @@
+/// <reference path="../references.spec.ts" />
+
+module Lui.Directives.TableGrid.Test {
+
+	"use strict";
+
+	beforeEach(angular.mock.module("lui.directives"));
+
+	describe("Table Grid", () => {
+		it("bogus should fail", () => {
+			expect(false).toBeTruthy();
+		});
+	});
+}
