49a841e66b8c53a27133c14e031f99fe14d7dba0
lucienbertin <me@lucienbertin.com>
2016-02-17 16:15:10 +0100

    grunt [add] task ngtemplates included in grunt dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
 	// use this one when you're coding e2e tests
 	grunt.registerTask('e2e', ['concurrent:e2e']);
 	// this task updates all distributions - launch it once before each release
-	grunt.registerTask('dist', ["ts:dist", 'concat:spe', 'uglify:spe', 'concat:standard', 'uglify:standard', 'sass:dist']);
+	grunt.registerTask('dist', ["ts:dist", "ngtemplates:dist", 'concat:spe', 'uglify:spe', 'concat:standard', 'uglify:standard', 'sass:dist']);
 	// this updates the dists and tests it, creates karma coverage
 	grunt.registerTask('test', ['dist', "ts:test", 'karma:debug', 'karma:coverage', 'protractor:singlerun', 'jshint']);
 
