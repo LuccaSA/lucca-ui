6d340ea54f749c7e8238b32d8851e795eeeae4e0
lucienbertin <me@lucienbertin.com>
2016-03-18 11:55:53 +0100

    ptor - use version 3.1.1 cuz version 3.2.0 does not work with grunt yet

--- protractor.conf.js
+++ protractor.conf.js
@@ -7,15 +7,15 @@ exports.config = {
 	},
 
 	jasmineNodeOpts: {
-	// onComplete will be called just before the driver quits.
-	onComplete: null,
-	// If true, display spec names.
-	isVerbose: false,
-	// If true, print colors to the terminal.
-	showColors: true,
-	// If true, include stack traces in failures.
-	includeStackTrace: true,
-	// Default time to wait in ms before a test fails.
-	defaultTimeoutInterval: 100000
+		// onComplete will be called just before the driver quits.
+		onComplete: null,
+		// If true, display spec names.
+		isVerbose: false,
+		// If true, print colors to the terminal.
+		showColors: true,
+		// If true, include stack traces in failures.
+		includeStackTrace: true,
+		// Default time to wait in ms before a test fails.
+		defaultTimeoutInterval: 100000
 	}
 };
