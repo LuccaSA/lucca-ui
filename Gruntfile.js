6dc76d8d5bf4357668274f9ae5fbfd1508064cde
lucienbertin <me@lucienbertin.com>
2016-04-06 10:10:54 +0200

    dist [rem] tablegrid from the standard distribution of lucca-ui cuz it has dependencies to ui-select and pascal precht <3

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
 	grunt.initConfig(configs);
 
 	// this task updates all distributions - launch it once before each release
-	grunt.registerTask('dist', ["ts:dist", "ngtemplates:dist", 'concat:spe', 'uglify:spe', 'concat:standard', 'uglify:standard', 'sass:dist']);
+	grunt.registerTask('dist', ["ts:dist", "ts:distspe", "ngtemplates:dist", 'concat:spe', 'uglify:spe', 'concat:standard', 'uglify:standard', 'sass:dist']);
 
 	// use this tasks when you are developping
 	grunt.registerTask('debug', ["dist", "ts:test", "connect:server", 'concurrent:debug']);
