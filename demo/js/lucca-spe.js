7a31c39022b0a8bc68962a0211bb3e1504eed0b6
lucienbertin <me@lucienbertin.com>
2016-05-18 11:50:32 +0200

    demo [upd] add section for method notify.loading

--- demo/js/lucca-spe.js
+++ demo/js/lucca-spe.js
@@ -91,7 +91,7 @@
 		}
 	}]);
 	angular.module("demoApp")
-	.controller("notifyCtrl", ["$scope", "luisNotify", function($scope, luisNotify){
+	.controller("notifyCtrl", ["$scope", "luisNotify", "$q", function($scope, luisNotify, $q){
 		$scope.message = "this is a notification";
 		$scope.details = "open console with f12 to witness the logging";
 		$scope.notifyError = function(message, details) {
@@ -103,6 +103,32 @@
 		$scope.notifySuccess = function(message, details) {
 			luisNotify.success(message, details);
 		};
+		$scope.loadingMessage = "doing a long treatment";
+		$scope.resolvedMessage = "It worked!";
+		$scope.rejectedMessage = "It failed :'(";
+		$scope.canceledMessage = "It was taking too long";
+		var cancel = function() {
+			luisNotify.warning("The user clicked the x before the treatment was done");
+			$scope.loading = false;
+		}
+		var loadingDfd;
+		$scope.notifyLoading = function(message) {
+			loadingDfd = $q.defer();
+			$scope.loading = true;
+			luisNotify.loading(loadingDfd.promise, message, cancel);
+		};
+		$scope.changeLoading = function(message) {
+			loadingDfd.notify(message);
+		};
+		$scope.resolveLoading = function(message) {
+			$scope.loading = false;
+			loadingDfd.resolve(message);
+		};
+		$scope.rejectLoading = function(message) {
+			$scope.loading = false;
+			loadingDfd.reject(message);
+		};
+
 		$scope.message2 = "are you really sure?";
 		$scope.notifyAlert = function(message) {
 			luisNotify.alert(message)
