e2ca30e1b6346e4867ec39ee13bfa5c2328abe98
Benoit PAUGAM <bpaugam@lucca.fr>
2016-05-19 23:21:25 +0200

    Updated gruntfile for lui-build

--- config/lui-build.js
+++ config/lui-build.js
@@ -2,16 +2,13 @@
 
 module.exports = function(grunt, options){
 	return {
-		options:{
-			build: 'scss/themes/default/build.json',
-		},
-		dist:{
-			files: [
-				{ 'scss/themes/default/theme.json': ['scss/themes/default/core/**/*.yml'] },
-				// { 'scss/themes/default/references.json': ['scss/themes/default/core/references/**/*.yml'] },
-				// { 'scss/themes/default/adjectives.json': ['scss/themes/default/core/adjectives/**/*.yml'] },
-				// { 'scss/themes/default/elements.json': ['scss/themes/default/core/elements/**/*.yml'] },
-			],
+		dist: {
+			options: {
+				base: process.cwd(),
+				custom: null,
+				build: 'scss/themes/default/build.json',
+				dest: 'build/lucca-ui.build.scss'
+			}
 		}
 	};
 };
