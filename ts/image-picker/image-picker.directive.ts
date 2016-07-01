6313c3998e186dde3ebd4c00d5e9a803962194f0
lucienbertin <me@lucienbertin.com>
2016-07-01 17:23:02 +0200

    imae-picker [add] luid-image-picker that displays a file

---
+++ ts/image-picker/image-picker.directive.ts
@@ -0,0 +1,70 @@
+module Lui.Directives {
+	"use strict";
+	class LuidImagePicker implements angular.IDirective {
+		public static IID: string = "luidImagePicker";
+		public restrict = "E";
+		public templateUrl = "lui/templates/image-picker/image-picker.html";
+		public require = ["ngModel", LuidImagePicker.IID];
+		public scope = {
+			placeholder: "@",
+		};
+		public controller: string = LuidImagePickerController.IID;
+		public static factory(): angular.IDirectiveFactory {
+			let directive = () => {
+				return new LuidImagePicker();
+			};
+			return directive;
+		}
+		public link(scope: IImagepickerScope, element: angular.IAugmentedJQuery, attrs: angular.IAttributes, ctrls: any[]): void {
+			let ngModelCtrl = <ng.INgModelController>ctrls[0];
+			let imgPickerCtrl = <LuidImagePickerController>ctrls[1];
+			imgPickerCtrl.setNgModelCtrl(ngModelCtrl);
+			imgPickerCtrl.setPlaceholder(scope.placeholder);
+		}
+	}
+
+	export interface IFile {
+		id?: string;
+		name?: string;
+		href: string;
+	}
+	interface IImagepickerScope extends ng.IScope {
+		pictureStyle: { "background-image": string };
+		placeholder: string;
+	}
+
+
+	class LuidImagePickerController {
+		public static IID: string = "luidImagePickerController";
+		public static $inject: Array<string> = ["$scope"];
+		private $scope: IImagepickerScope;
+		private ngModelCtrl: ng.INgModelController;
+		private placeholder: string;
+
+		constructor($scope: IImagepickerScope) {
+			this.$scope = $scope;
+		}
+		// set stuff - is called in the linq function
+		public setNgModelCtrl(ngModelCtrl: ng.INgModelController): void {
+			this.ngModelCtrl = ngModelCtrl;
+			ngModelCtrl.$render = () => {
+				let vv = this.getViewValue();
+				if (!!vv && !!vv.href) {
+					this.$scope.pictureStyle = { "background-image": "url('" + vv.href + "')" };
+				} else {
+					this.$scope.pictureStyle = { "background-image": "url('" + this.placeholder + "')" };
+				}
+			};
+		}
+		public setPlaceholder(placehoilder: string): void {
+			this.placeholder = placehoilder || "/static/common/images/placeholder-pp.png";
+		}
+
+		private getViewValue(): IFile {
+			return <IFile>this.ngModelCtrl.$viewValue;
+		}
+	}
+
+	angular.module("lui.directives").directive(LuidImagePicker.IID, LuidImagePicker.factory());
+	angular.module("lui.directives").controller(LuidImagePickerController.IID, LuidImagePickerController);
+}
