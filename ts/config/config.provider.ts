8134e2c5e15ceb6a021730a4702e4aa0153fbd33
lucienbertin <me@lucienbertin.com>
2016-07-04 15:28:41 +0200

    services [add] a config provider

---
+++ ts/config/config.provider.ts
@@ -0,0 +1,58 @@
+module Lui {
+	"use strict";
+	export interface IConfig {
+		parentTagIdClass?: string;
+		parentElt?: ng.IAugmentedJQuery;
+		prefix?: string;
+		startTop?: number;
+		okLabel?: string;
+		cancelLabel?: string;
+		canDismissConfirm?: boolean;
+		cropLabel?: string;
+		noCropLabel?: string;
+	}
+}
+module Lui.Service {
+	"use strict";
+	class LuipConfig {
+		public config: IConfig = {};
+		public $get = ["$log", ($log: ng.ILogService): Config => {
+			return new Config($log, this.config);
+		}];
+	}
+	class Config implements IConfig {
+		public parentTagIdClass: string;
+		public parentElt: ng.IAugmentedJQuery;
+		public prefix: string;
+		public startTop: number;
+		public okLabel: string;
+		public cancelLabel: string;
+		public canDismissConfirm: boolean;
+		constructor($log: ng.ILogService, conf: IConfig) {
+			_.extend(this, conf);
+			// find the parent element where we'll append all modals
+			if (!this.parentElt && !!this.parentTagIdClass) {
+				let parentTagIdClass = this.parentTagIdClass || "body";
+				let byTag = document.getElementsByTagName(parentTagIdClass);
+				let byId = document.getElementById(parentTagIdClass);
+				let byClass = document.getElementsByClassName(parentTagIdClass);
+				if (!!byTag && byTag.length) {
+					this.parentElt = angular.element(byTag[0]);
+				} else if (!!byId) {
+					this.parentElt = angular.element(byId);
+				} else if (!!byClass && byClass.length) {
+					this.parentElt = angular.element(byClass[0]);
+				} else {
+					$log.warn("luisConfig - could not find a suitable element for tag/id/class: " + parentTagIdClass);
+				}
+			}
+			// default values
+			this.prefix = this.prefix || "lui";
+			this.startTop = this.startTop || 40;
+			this.okLabel = this.okLabel || "Ok";
+			this.cancelLabel = this.cancelLabel || "Cancel";
+			this.canDismissConfirm = this.canDismissConfirm;
+		}
+	}
+	angular.module("lui.services").provider("luisConfig", LuipConfig);
+}
