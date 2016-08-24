2f58c831ca3cb745cc0399a3c6fbd02c9fe8f945
lucienbertin <me@lucienbertin.com>
2016-08-24 11:20:17 +0200

    formly [add] text type

--- ts/formly/fields/fields.config.ts
+++ ts/formly/fields/fields.config.ts
@@ -6,29 +6,29 @@ module dir.directive {
 			name: "text",
 			templateUrl: "lui/templates/formly/fields/text.html",
 		});
-		formlyConfigProvider.setType({
-			name: "email",
-			templateUrl: "lui/templates/formly/fields/email.html",
-		});
-		formlyConfigProvider.setType({
-			name: "date",
-			templateUrl: "lui/templates/formly/fields/date.html",
-		});
-		formlyConfigProvider.setType({
-			name: "date_minmax",
-			templateUrl: "lui/templates/formly/fields/date-minmax.html",
-		});
-		formlyConfigProvider.setType({
-			name: "user",
-			templateUrl: "lui/templates/formly/fields/user.html",
-		});
-		formlyConfigProvider.setType({
-			name: "api_select",
-			templateUrl: "lui/templates/formly/fields/api-select.html",
-		});
-		formlyConfigProvider.setType({
-			name: "picture",
-			templateUrl: "lui/templates/formly/fields/picture.html",
-		});
+		// formlyConfigProvider.setType({
+		// 	name: "email",
+		// 	templateUrl: "lui/templates/formly/fields/email.html",
+		// });
+		// formlyConfigProvider.setType({
+		// 	name: "date",
+		// 	templateUrl: "lui/templates/formly/fields/date.html",
+		// });
+		// formlyConfigProvider.setType({
+		// 	name: "date_minmax",
+		// 	templateUrl: "lui/templates/formly/fields/date-minmax.html",
+		// });
+		// formlyConfigProvider.setType({
+		// 	name: "user",
+		// 	templateUrl: "lui/templates/formly/fields/user.html",
+		// });
+		// formlyConfigProvider.setType({
+		// 	name: "api_select",
+		// 	templateUrl: "lui/templates/formly/fields/api-select.html",
+		// });
+		// formlyConfigProvider.setType({
+		// 	name: "picture",
+		// 	templateUrl: "lui/templates/formly/fields/picture.html",
+		// });
 	}]);
 }
