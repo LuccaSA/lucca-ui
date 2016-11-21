2195884d58a024f6057f23bd748fdb471c43ad60
Riron <orioncharlier@gmail.com>
2016-11-21 11:51:56 +0100

    ng2 FIX: Export manquant
    Les classes utilisées en dehors du fichier doivent être exportées, sinon on a une erreur au moment du `ngc`

--- ng2/src/components/user-picker/user-picker.sevice.ts
+++ ng2/src/components/user-picker/user-picker.sevice.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-class Options {
+export class Options {
 	showFormerEmployees: boolean;
 	clue: string;
 	appId: number;
