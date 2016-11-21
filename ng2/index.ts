cb76868711c6c5c3ef04f1809d8f16bd4fd1dcfb
Maxime Mg <mmangin@lucca.fr>
2016-11-21 17:25:43 +0100

    ng2 REF: /index

--- ng2/index.ts
+++ ng2/index.ts
@@ -1,4 +1,4 @@
 export { LuiUserPickerModule } from './src/components/user-picker/user-picker.module';
-export { LuiProgressBarModule } from './src/components/progress-bar/index';
+export { LuiProgressBarModule } from './src/components/progress-bar';
 
-export { LuiTimePipesModule } from './src/pipes/time/index';
+export { LuiTimePipesModule } from './src/pipes/time';
