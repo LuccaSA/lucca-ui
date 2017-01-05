fb066417723feedf7625142e3f1444727d4589da
Clotilde Martin-Lemerle <Clotilde@iMac-de-Clotilde.local>
2017-01-05 11:18:02 +0100

    refonte.demo [anims] done

--- demo/animations/enter/enter.js
+++ demo/animations/enter/enter.js
@@ -2,12 +2,18 @@
 	'use strict';
 
 	angular.module('demoApp')
-	.controller("enterCtrl", ["$scope", function($scope){
+	.controller("enterCtrl", ["$scope", "$timeout", function($scope, $timeout){
 		$scope.array = [];
 		$scope.add = function(){
 			$scope.array.push($scope.animation +  " from " + $scope.direction);
+			$scope.showButton = false;
+			$timeout(function() {
+				$scope.showButton = true;
+			}, 1);
 		};
 		$scope.direction = "up";
 		$scope.animation = "fade in";
+
+		$scope.showButton = false;
 	}]);
 })();
