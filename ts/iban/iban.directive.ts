7034080439d50dd188c874e39d1f28159ebdce37
lucienbertin <me@lucienbertin.com>
2017-01-23 15:54:13 +0100

    ref iban

--- ts/iban/iban.directive.ts
+++ ts/iban/iban.directive.ts
@@ -1,4 +1,4 @@
-module Lui.Directives {
+module lui.iban {
 	"use strict";
 
 	export class LuidIban implements ng.IDirective {
--- ts/iban/select-next.directive.ts
+++ ts/iban/select-next.directive.ts
@@ -1,4 +1,4 @@
-module Lui.Directives {
+module lui.iban {
 	"use strict";
 
 	export class LuidSelectNext implements ng.IDirective {
