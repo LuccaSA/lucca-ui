d342aaeb66a7d695ca54658c8fb5c1296f1cd34c
lucienbertin <me@lucienbertin.com>
2017-04-04 11:55:40 +0200

    dist

--- dist/lucca-ui.d.ts
+++ dist/lucca-ui.d.ts
@@ -452,11 +452,11 @@ declare module lui.userpicker {
         id: number;
         firstName: string;
         lastName: string;
-        dtContractEnd: string;
-        hasLeft: boolean;
-        info: string;
-        hasHomonyms: boolean;
-        additionalProperties: IHomonymProperty[];
+        dtContractEnd?: string;
+        hasLeft?: boolean;
+        info?: string;
+        hasHomonyms?: boolean;
+        additionalProperties?: IHomonymProperty[];
     }
     interface IHomonymProperty {
         translationKey: string;
