99f7d80df01c926169b78e6a25abbd9663918cb9
Bastien Guillon <bguillon@lucca.fr>
2017-04-19 17:02:55 +0200

    clean code

--- js/directives/lucca/translations-input.js
+++ js/directives/lucca/translations-input.js
@@ -1,166 +1,146 @@
-(function(){
-	'use strict';
-		/**
-	** DEPENDENCIES
-	**  - angular translate
-	**  - underscore
-	**/
+(function () {
+	"use strict";
 
-	angular.module('lui.translate')
-	.directive('luidTranslations', ['$translate', '_', '$filter', '$timeout', function($translate, _, $filter,  $timeout){
-		function link(scope, element, attrs, ctrls){
-			var ngModelCtrl = ctrls[1];
-			var translateCtrl = ctrls[0];
+	/**
+	 ** DEPENDENCIES
+	 **  - angular translate
+	 **  - underscore
+	 **/
 
-			var cultures = ['en', 'de', 'es', 'fr', 'it', 'nl'];
-			scope.cultures = cultures;
-			// need the current culture to
-			var currentCulture = $translate.preferredLanguage() || "en";
-			scope.currentCulture = currentCulture;
+	angular.module("lui.translate")
+		.directive("luidTranslations", ["$translate", "_", "$filter", "$timeout", function ($translate, _, $filter, $timeout) {
+			function link(scope, element, attrs, ctrls) {
+				var ngModelCtrl = ctrls[1];
+				var translateCtrl = ctrls[0];
 
-			// default mode is dictionary
-			var mode = 'dictionary';
-			if(!!attrs.mode){
-				mode = scope.mode;
-			}
-			if(mode === 'dictionary' && ngModelCtrl.$viewValue !== undefined){
-				_.each(cultures, function(c){
-					scope.$watch(function () { return !!ngModelCtrl.$viewValue ? ngModelCtrl.$viewValue[c] : ngModelCtrl.$viewValue; },
-					function () { ngModelCtrl.$render(); });
-				});
-			}
+				// Available cultures
+				var cultures = ["en", "de", "es", "fr", "it", "nl"];
+				scope.cultures = cultures;
 
-			ngModelCtrl.$render = function(){
-				scope.internal = parse(ngModelCtrl.$viewValue);
-			};
-			translateCtrl.updateViewValue = function(){
-				switch(mode){
-					case "dictionary":
-						return updateDictionary(scope.internal);
-					case "|":
-					case "pipe":
-						return updatePipe(scope.internal);
-					case "[]":
-					case "brackets":
-						return updateBrackets(scope.internal);
+				scope.currentCulture = $translate.preferredLanguage() || "en";
+
+				var mode = !!attrs.mode ? attrs.mode : "dictionary";
+				if (mode === "dictionary" && ngModelCtrl.$viewValue !== undefined) {
+					_.each(cultures, function (c) {
+						scope.$watch(
+							function () { return !!ngModelCtrl.$viewValue ? ngModelCtrl.$viewValue[c] : ngModelCtrl.$viewValue; },
+							function () { ngModelCtrl.$render(); }
+						);
+					});
 				}
-			};
 
-			var parse = function (value) {
-				if (value === undefined) { return undefined;}
-				switch(mode){
-					case "dictionary":
-						return parseDictionary(value);
-					case "|":
-					case "pipe":
-						return parsePipe(value);
-					case "[]":
-					case "brackets":
-						return parseBrackets(value);
-					default:
-						return undefined;
-				}
-			};
+				ngModelCtrl.$render = function () { scope.internal = parse(ngModelCtrl.$viewValue); };
+				translateCtrl.updateViewValue = function () {
+					switch (mode) {
+						case "dictionary":
+							return updateDictionary(scope.internal);
+						case "|":
+						case "pipe":
+							return updatePipe(scope.internal);
+					}
+				};
 
-			// mode dictionary
-			var parseDictionary = function(value){
-				return _.reduce(cultures, function(memo, c){
-					memo[c] = value[c];
-					return memo;
-				}, {});
-			};
-			var updateDictionary = function(value){
-				var allEmpty = true;
-				var viewValue = {};
-				_.each(cultures, function (culture) {
-					viewValue[culture] = value[culture];
-					allEmpty &= value[culture] === undefined || value[culture] === "";
-				});
-				ngModelCtrl.$setViewValue(allEmpty ? undefined : viewValue);
-				scope.$parent.$eval(attrs.ngChange); // needs to be called manually cuz the object ref of the $viewValue didn't change
-			};
+				var parse = function (value) {
+					if (value === undefined) { return undefined; }
+					switch (mode) {
+						case "dictionary":
+							return parseDictionary(value);
+						case "|":
+						case "pipe":
+							return parsePipe(value);
+						default:
+							return undefined;
+					}
+				};
 
-			// mode pipe
-			var parsePipe = function(value){
-				// value looks like this "en:some stuff|de:|nl:|fr:des bidules|it:|es:"
-				var translations = value.split("|");
-				var result = {};
-				_.each(translations, function(t){
-					var key = t.substring(0,2);
-					var val = t.substring(3);
-					result[key] = val;
-				});
-				return _.pick(result, cultures);
-			};
-			var updatePipe = function(value) {
-				if (!_.find(cultures, function (culture) { return value[culture] !== undefined && value[culture] !== ""; })) {
-					ngModelCtrl.$setViewValue(undefined);
-				} else {
-					var newVal = _.map(cultures, function (c) {
-						if (!!value[c]) {
-							return c + ":" + value[c];
-						}
-						return c + ":";
-					}).join("|");
-					ngModelCtrl.$setViewValue(newVal);
-				}
-			};
+				// Mode dictionary
+				var parseDictionary = function (value) {
+					return _.reduce(cultures, function (memo, c) {
+						memo[c] = value[c];
+						return memo;
+					}, {});
+				};
+				var updateDictionary = function (value) {
+					var allEmpty = true;
+					var viewValue = {};
+					_.each(cultures, function (culture) {
+						viewValue[culture] = value[culture];
+						allEmpty &= value[culture] === undefined || value[culture] === "";
+					});
+					ngModelCtrl.$setViewValue(allEmpty ? undefined : viewValue);
+					scope.$parent.$eval(attrs.ngChange); // needs to be called manually cuz the object ref of the $viewValue didn't change
+				};
 
-			// mode brackets
-			var parseBrackets = function(value){
-				return {};
-			};
-
-		}
-		return{
-			require:['luidTranslations','^ngModel'],
-			controller:'luidTranslationsController',
-			scope: {
-				// disabled:'=',
-
-				mode:'@', // allowed values: "|" "pipe", "[]" "brackets", "dictionary"
-
-				size:"@", // the size of the input (short, long, x-long, fitting)
-			},
-			templateUrl:"lui/directives/luidTranslations.html",
-			restrict:'EA',
-			link:link
-		};
-	}])
-	.controller('luidTranslationsController', ['$scope', '$translate', '$timeout', function($scope, $filter, $timeout){
-		var ctrl = this;
-		/******************
-		* UPDATE          *
-		******************/
-		$scope.update = function(){
-			ctrl.updateViewValue();
-		};
-
-
-		/******************
-		* FOCUS & BLUR    *
-		******************/
-		var blurTimeout;
-		$scope.focusInput = function(){
-			if(!!blurTimeout){
-				$timeout.cancel(blurTimeout);
-				blurTimeout = undefined;
+				// Mode pipe
+				var parsePipe = function (value) {
+					// value looks like this "en:some stuff|de:|nl:|fr:des bidules|it:|es:"
+					var translations = value.split("|");
+					var result = {};
+					_.each(translations, function (t) {
+						var key = t.substring(0, 2);
+						var val = t.substring(3);
+						result[key] = val;
+					});
+					return _.pick(result, cultures);
+				};
+				var updatePipe = function (value) {
+					if (!_.find(cultures, function (culture) { return value[culture] !== undefined && value[culture] !== ""; })) {
+						ngModelCtrl.$setViewValue(undefined);
+					} else {
+						var newVal = _.map(cultures, function (c) {
+							if (!!value[c]) {
+								return c + ":" + value[c];
+							}
+							return c + ":";
+						}).join("|");
+						ngModelCtrl.$setViewValue(newVal);
+					}
+				};
 			}
-			$scope.focused = true;
-		};
-		$scope.blurInput = function(){
-			blurTimeout = $timeout(function(){
-				$scope.focused = false;
-			}, 500);
-		};
+			return {
+				require: ['luidTranslations', '^ngModel'],
+				controller: 'luidTranslationsController',
+				scope: {
+					mode: '@', // allowed values: "|" "pipe", "dictionary"
+					size: "@", // the size of the input (short, long, x-long, fitting)
+				},
+				templateUrl: "lui/directives/luidTranslations.html",
+				restrict: 'EA',
+				link: link
+			};
+		}])
+		.controller('luidTranslationsController', ['$scope', '$translate', '$timeout', function ($scope, $filter, $timeout) {
+			var ctrl = this;
+			/******************
+			* UPDATE          *
+			******************/
+			$scope.update = function () {
+				ctrl.updateViewValue();
+			};
 
-	}]);
+			/******************
+			* FOCUS & BLUR    *
+			******************/
+			var blurTimeout;
+			$scope.focusInput = function () {
+				if (!!blurTimeout) {
+					$timeout.cancel(blurTimeout);
+					blurTimeout = undefined;
+				}
+				$scope.focused = true;
+			};
+			$scope.blurInput = function () {
+				blurTimeout = $timeout(function () {
+					$scope.focused = false;
+				}, 500);
+			};
 
+		}]);
 
 	/**************************/
-	/***** TEMPLATEs      *****/
+	/***** TEMPLATES      *****/
 	/**************************/
-	angular.module("lui").run(["$templateCache", function($templateCache) {
+	angular.module("lui").run(["$templateCache", function ($templateCache) {
 		$templateCache.put("lui/directives/luidTranslations.html",
 			"<div class=\"lui dropdown {{size}} field\" ng-class=\"{open:focused || hovered}\" ng-mouseenter=\"hovered=true\" ng-mouseleave=\"hovered=false\">" +
 			"	<div class=\"lui input\">" +
@@ -174,11 +154,8 @@
 			"				<span class=\"unit addon\">{{culture}}</span>" +
 			"			</div>" +
 			"		</div>" +
-			// "		<hr>" +
-			// "		<div class=\"lui button right pulled\" ng-click=\"close()\">Ok</div>" +
 			"	</div>" +
 			"</div>" +
-		"");
-
+			"");
 	}]);
 })();
