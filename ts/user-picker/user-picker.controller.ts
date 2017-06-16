211094d74004785fc6801a6663b61359dd2bfc52
pierop <apieropan@lucca.fr>
2017-06-16 11:29:16 +0200

    user-picker (enh] when includeFormerEmployees changes, call refresh with previous search

--- ts/user-picker/user-picker.controller.ts
+++ ts/user-picker/user-picker.controller.ts
@@ -36,6 +36,7 @@ module lui.userpicker {
 
 		/** Indicates if the controller controls a user-picker or a user-picker-multiple directive */
 		private multiple: boolean;
+		private clue: string;
 
 		constructor(
 			$scope: ILuidUserPickerScope,
@@ -79,7 +80,7 @@ module lui.userpicker {
 		 * Initialize the functions of the scope.
 		 */
 		private initializeScope(): void {
-			this.$scope.$watch("displayMeFirst", (newValue: boolean, oldValue) => {
+			this.$scope.$watch("displayMeFirst", (newValue: boolean, oldValue: boolean) => {
 				if (this.$scope.displayMeFirst) {
 					if (newValue) {
 						let myIndex = _.findIndex(this.$scope.users, (user: IUserLookup) => { return user.id === this.$scope.myId; });
@@ -102,7 +103,7 @@ module lui.userpicker {
 				if (newValue !== oldValue) {
 					this.$scope.$broadcast("toggleFormerEmployees");
 					this.resetUsers();
-					this.refresh();
+					this.refresh(this.clue);
 				}
 			});
 
@@ -130,6 +131,7 @@ module lui.userpicker {
 			});
 
 			this.$scope.find = (search: string): void => {
+				this.clue = search;
 				this.resetUsers();
 				this.refresh(search);
 			};
