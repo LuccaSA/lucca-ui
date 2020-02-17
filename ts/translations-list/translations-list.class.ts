f415031697256442213cca125eac4d6c9aaeb782
maxmg <maxime.mangin@gmail.com>
2020-02-17 09:44:02 +0100

    ADD: porteguese in luid-translations component

--- ts/translations-list/translations-list.class.ts
+++ ts/translations-list/translations-list.class.ts
@@ -2,11 +2,11 @@ module lui.translate {
 	"use strict";
 
 	/** All the languages supported by the translation-list directive */
-	export const AVAILABLE_LANGUAGES = ["en", "fr", "de", "es", "it", "nl"];
+	export const AVAILABLE_LANGUAGES = ["en", "fr", "de", "es", "it", "nl", "pt"];
 	/** Used to convert languages labels to their associated culture codes */
-	export const LANGUAGES_TO_CODE = { en: 1033, de: 1031, es: 1034, fr: 1036, it: 1040, nl: 2067 };
+	export const LANGUAGES_TO_CODE = { en: 1033, de: 1031, es: 1034, fr: 1036, it: 1040, nl: 2067, pt: 2070 };
 	/** Used to converted culture codes to their associated labels  */
-	export const CODES_TO_LANGUAGES = { 1033: "en", 1031: "de", 1034: "es", 1036: "fr", 1040: "it", 2067: "nl" };
+	export const CODES_TO_LANGUAGES = { 1033: "en", 1031: "de", 1034: "es", 1036: "fr", 1040: "it", 2067: "nl", 2070: "pt" };
 
 	export class CulturedList {
 		public culture: string;
