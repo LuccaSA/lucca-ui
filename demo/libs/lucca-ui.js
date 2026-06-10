b79d7eb8702f57ba9f331237bd64e004a44cbcfa
Natalia Sugrobova <nsugrobova@luccasoftware.com>
2026-06-10 18:34:38 +0200

    feat(polish) Add polish language support in translations input

--- demo/libs/lucca-ui.js
+++ demo/libs/lucca-ui.js
@@ -3053,9 +3053,9 @@ var lui;
     var translate;
     (function (translate) {
         "use strict";
-        translate.AVAILABLE_LANGUAGES = ["en", "fr", "de", "es", "it", "nl", "pt"];
-        translate.LANGUAGES_TO_CODE = { en: 1033, de: 1031, es: 1034, fr: 1036, it: 1040, nl: 1043, pt: 2070 };
-        translate.CODES_TO_LANGUAGES = { 1033: "en", 1031: "de", 1034: "es", 1036: "fr", 1040: "it", 1043: "nl", 2070: "pt" };
+        translate.AVAILABLE_LANGUAGES = ["en", "fr", "de", "es", "it", "nl", "pt", "pl"];
+        translate.LANGUAGES_TO_CODE = { en: 1033, de: 1031, es: 1034, fr: 1036, it: 1040, nl: 1043, pt: 2070, pl: 1045 };
+        translate.CODES_TO_LANGUAGES = { 1033: "en", 1031: "de", 1034: "es", 1036: "fr", 1040: "it", 1043: "nl", 2070: "pt", 1045: "pl" };
         var CulturedList = (function () {
             function CulturedList(culture) {
                 this.culture = culture;
@@ -4502,7 +4502,7 @@ var lui;
 				var translateCtrl = ctrls[0];
 
 				/** Associations language/code */
-				var languagesToCodes = { en: 1033, de: 1031, es: 1034, fr: 1036, it: 1040, nl: 1043, pt: 2070 };
+				var languagesToCodes = { en: 1033, de: 1031, es: 1034, fr: 1036, it: 1040, nl: 1043, pt: 2070, pl: 1045 };
 
 				/** List of all the available languages labels */
 				var cultures = _.keys(languagesToCodes);
